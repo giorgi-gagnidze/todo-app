b918c7ad799f87e27ae2799cb62be11c7b5f4070
siduki <giorgigagnidze@gmail.com>
2023-04-17 01:15:15 +0400

    create function to add item in list

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const btnAdd = document.querySelector(".btn-add");
+
+function newElement() {
+  let li = document.createElement("li");
+  let inputValue = document.querySelector(".todo-input").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    console.log("Input is empty");
+  } else {
+    document.querySelector(".myList").appendChild(li);
+  }
+  console.log(inputValue);
+}
+
+btnAdd.addEventListener("click", newElement);
